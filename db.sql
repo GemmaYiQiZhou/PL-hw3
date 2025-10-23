001d4e6ceb2982bd6c329e49d17304eacd64cad2
GemmaYiQiZhou <gsm3cx@virginia.edu>
2025-10-23 18:13:59 -0400

    trying to set db

---
+++ db.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS hw3_users;
+
+CREATE TABLE hw3_users (
+    id INT PRIMARY KEY,
+    username VARCHAR(255),
+    email VARCHAR(255),
+    pass VARCHAR(255),
+);
---
+++ views/welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Welcome Page</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+
+<body>
+    <h1>Welcome</h1>
+</body>
+
+</html>
